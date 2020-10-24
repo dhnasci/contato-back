c6e03f7249486fcabbba3b156c6c969b2fe8693e
Dirceu Henrique <dhnasci@hotmail.com>
2020-10-23 22:39:05 -0400

    Testando Rest Contato

--- src/main/java/br/com/creathus/contato/resources/ContatoResource.java
+++ src/main/java/br/com/creathus/contato/resources/ContatoResource.java
@@ -1,16 +1,28 @@
 package br.com.creathus.contato.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.creathus.contato.domain.Contato;
+import br.com.creathus.contato.domain.enums.Sexo;
 
 @RestController
 @RequestMapping("/contatos")
 public class ContatoResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando...";
+	public List<Contato> listar(){	
+		List<Contato> lista = new ArrayList();
+		Contato cont1 = new Contato(1,"Ronaldo Silva", Sexo.MASCULINO, "3234-3344", "ronaldo@gmail.com");
+		Contato cont2 = new Contato(2, "Sonia Pereira", Sexo.FEMININO, "3234-2233", "sonia@cret.com.br");
+		
+		lista.add(cont1);
+		lista.add(cont2);
+		return lista;
 	}
 
 }
